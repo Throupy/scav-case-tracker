c4468ce5724c09b1899bc4ae403a5e739800614e
owen <owenthroup@gmail.com>
2024-10-13 21:42:53 +0100

    updated entry frontend, now using wtforms for submission too.

---
+++ app/main/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, FileField, HiddenField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+
+class ScavCaseForm(FlaskForm):
+    scav_case_type = SelectField(
+        "Scav Case Type",
+        choices = [
+            ("₽2500", "₽2500"),
+            ("₽15000", "₽15000"),
+            ("₽95000", "₽95000"),
+            ("Moonshine", "Moonshine"),
+            ("Intelligence", "Intelligence"),
+        ],
+        validators=[DataRequired()]        
+    )
+
+    items_data = HiddenField("Items Data")
+    scav_case_image = FileField("Upload an Image")
+    submit = SubmitField("Submit Scav Case")
+
+    def validate_items_data(form, field):
+        if not form.scav_case_image.data:
+            if not field.data or field.data == '[]':
+                raise ValidationError(
+                    "You must select at least one item for a scav case"
+                )
